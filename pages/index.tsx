b5919d46b4bcdf28246ed647e216af6306135ffb
amberchiu <suzumiya1235@gmail.com>
2023-03-01 23:25:28 +0800

    style(*) add normalize CSS and background noise effect.

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+
+import '../styles/globals.css'
+import style from '../styles/index.module.scss'
+
+export default function Home() {
+  return (
+    <div className={style.main}>
+        heloow
+    </div>
+  )
+}
