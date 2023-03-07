9e572ad2aaa2aaf7a66edf28b3e7a12ec6aab7e2
amberchiu <suzumiya1235@gmail.com>
2023-03-07 23:56:32 +0800

    feat(pages) add header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import './header.scss'
 
-interface HeaderProps {
-    color?: string
-}
-
-const Header = (props: HeaderProps):JSX.Element => {
+const Header = () => {
   return (
     <div className='header'>
-      <a>HOME</a>
-      <a>WORK</a>
-      <a>ABOUT</a>
-      <a>CONTACT</a>
+      <h2>Hello, nihao!</h2>
+      <h1>I am Yu Fang Chiu</h1>
+      <h1>But you can call me Amber</h1>
+      <h2>I am a software engineer</h2>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-
 import '../styles/globals.css'
 import style from '../styles/index.module.scss'
+import Nav from '@/app/components/Nav'
 import Header from '@/app/components/Header'
 
 export default function Home() {
-
   return (
     <div className={style.main}>
       <>
+        <Nav />
         <Header />
       </>
     </div>
