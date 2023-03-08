9a0bb0416de8b8d40701489ae550cb51d3ee4f9a
amberchiu <suzumiya1235@gmail.com>
2023-03-09 01:10:45 +0800

    feat(pages) add preloading

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,43 @@ import '../styles/globals.css'
 import style from '../styles/index.module.scss'
 import Nav from '@/app/components/Nav'
 import Header from '@/app/components/Header'
+import Cursor from '@/app/components/cursor/CustomCursor'
+import CursorManager from '@/app/components/cursor/CursorManager'
+import { useState, useRef, useEffect } from 'react'
 
 export default function Home() {
+  const [preloading, setPreloading] = useState<boolean>(true)
+  const intervalRef: { current: NodeJS.Timeout | null } = useRef(null)
+  const clear = () => {
+    clearInterval(intervalRef.current as NodeJS.Timeout)
+  }
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setPreloading(false)
+    }, 3000)
+    intervalRef.current = id
+  }, [])
+
+  useEffect(() => {
+    if (!preloading) {
+      clear()
+    }
+  }, [preloading])
+
   return (
-    <div className={style.main}>
-      <>
-        <Nav />
-        <Header />
-      </>
-    </div>
+    <CursorManager>
+      {preloading ? (
+        <div>hello</div>
+      ) : (
+        <div className={style.main}>
+          <>
+            <Cursor />
+            <Nav />
+            <Header />
+          </>
+        </div>
+      )}
+    </CursorManager>
   )
 }
