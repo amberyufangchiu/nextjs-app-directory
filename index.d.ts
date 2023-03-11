38936732863b0f724c5394f7805a04f1a87d27a7
amberchiu <suzumiya1235@gmail.com>
2023-03-11 23:41:30 +0800

    feat(*) declare next module

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,16 @@
 declare module 'three'
+
+import { NextPageWithLayout } from 'next'
+import { AppProps } from 'next/app'
+
+declare module 'next' {
+  type NextPageWithLayout = NextPage & {
+    getLayout?: (page: ReactElement) => ReactNode
+  }
+}
+
+declare module 'next/app' {
+  type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
+  }
+}
