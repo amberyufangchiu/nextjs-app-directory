1eb575535f005b83e1ad2cd47f09060a925a3653
amberchiu <suzumiya1235@gmail.com>
2023-03-18 23:43:21 +0800

    feat(contact) add form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,82 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import './index.scss'
 
 const Contact = () => {
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const [formSuccess, setFormSuccess] = useState<boolean>(false)
+  const [formSuccessMessage, setFormSuccessMessage] = useState<string>("")
+
+  const handleInput = (e: any) => {
+    const fieldName = e.target.name;
+    const fieldValue = e.target.value;
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [fieldName]: fieldValue
+    }));
+  }
+
+  const submitForm = (e: any) => {
+    // We don't want the page to refresh
+    e.preventDefault()
+
+    const formURL = e.target.action
+    const data = new FormData()
+
+    // Turn our formData state into data we can use with a form submission
+    Object.entries(formData).forEach(([key, value]) => {
+      data.append(key, value);
+    })
+
+    // POST the data to the URL of the form
+    fetch(formURL, {
+      method: "POST",
+      body: data,
+      headers: {
+        'accept': 'application/json',
+      },
+    }).then((response) => response.json())
+    .then((data) => {
+      setFormData({ 
+        name: "", 
+        email: "", 
+        message: "" 
+      })
+
+      setFormSuccess(true)
+      setFormSuccessMessage(data.submission_text)
+    })
+  }
+
   return (
     <div className='Contact'>
-      
+      <h1>CONTACT</h1>
+      <div className='divider'></div>
+      <form method="POST" action="" onSubmit={submitForm}>
+          <div>
+            <label>Name</label>
+            <input type="text" name="name" onChange={handleInput} value={formData.name} />
+          </div>
+
+          <div>
+            <label>Email</label>
+            <input type="text" name="email" onChange={handleInput} value={formData.email} />
+          </div>
+
+          <div>
+            <label>Message</label>
+            <textarea name="message" onChange={handleInput} value={formData.message}></textarea>
+          </div>
+
+          <button type="submit">Send message</button>
+        </form>
     </div>
   )
 }
