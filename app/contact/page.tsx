9b80d126d93a0871abe74515b42c9bd7abb7363b
amberchiu <suzumiya1235@gmail.com>
2023-03-25 18:34:49 +0800

    feat(page) add simple contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,67 +1,73 @@
 'use client'
-import React, { useState, useEffect } from 'react'
-import './index.scss'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
 import { sendContactForm } from '../lib/api'
+import './index.scss'
 
 const Contact = () => {
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    message: ""
-  });
+    name: '',
+    email: '',
+    message: '',
+  })
 
   const [formSuccess, setFormSuccess] = useState<boolean>(false)
-  const [formSuccessMessage, setFormSuccessMessage] = useState<string>("")
+  const [formSuccessMessage, setFormSuccessMessage] = useState<string>('')
 
   const handleInput = (e: any) => {
-    const fieldName = e.target.name;
-    const fieldValue = e.target.value;
+    const fieldName = e.target.name
+    const fieldValue = e.target.value
 
     setFormData((prevState) => ({
       ...prevState,
-      [fieldName]: fieldValue
-    }));
+      [fieldName]: fieldValue,
+    }))
   }
 
-  const submitForm = async(e: any) => {
+  const submitForm = async (e: any) => {
     e.preventDefault()
-    
+
     const data = new FormData()
-    
+
     // Turn our formData state into data we can use with a form submission
     Object.entries(formData).forEach(([key, value]) => {
-      data.append(key, value);
+      data.append(key, value)
     })
-    
+
     try {
-      await sendContactForm(data);
+      await sendContactForm(data)
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
   }
 
   return (
-    <div className='Contact'>
+    <div className='contact'>
       <h1>CONTACT</h1>
       <div className='divider'></div>
-      <form method="POST" action="" onSubmit={submitForm}>
-          <div>
-            <label>Name</label>
-            <input type="text" name="name" onChange={handleInput} value={formData.name} />
-          </div>
-
-          <div>
-            <label>Email</label>
-            <input type="text" name="email" onChange={handleInput} value={formData.email} />
-          </div>
-
-          <div>
-            <label>Message</label>
-            <textarea name="message" onChange={handleInput} value={formData.message}></textarea>
-
-          </div>
-          <button type="submit">Send message</button>
-        </form>
+      <div className='inner'>
+        <div className='mail'>
+          <Image src='/icon/email.svg' width={50} height={50} alt='email' />
+          <p>amber.yufangchiu@gmail.com</p>
+        </div>
+        <div className='social-media'>
+          <Link href='https://github.com/amberyufangchiu'>
+            <Image src='/icon/github.svg' width={50} height={50} alt='github' />
+          </Link>
+          <Link href='https://www.linkedin.com/in/amber-yufang-chiu/'>
+            <Image
+              src='/icon/linkedin.svg'
+              width={50}
+              height={50}
+              alt='linkedin'
+            />
+          </Link>
+          <Link href='https://medium.com/@amber.yufangchiu'>
+            <Image src='/icon/medium.svg' width={50} height={50} alt='medium' />
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
